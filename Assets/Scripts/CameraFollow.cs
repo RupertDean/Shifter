8cbfd8e8a69bba622ab87747c69510a8df9ed51a
Rupert Dean <deanrupert@outlook.com>
2021-04-26 16:43:09 +0100

    Added player including animations, added parallax background

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float smoothSpeed = 0.01f;
+    public Vector3 offset;
+
+    void LateUpdate()
+    {
+      Vector3 desired = new Vector3(target.position.x + offset.x, 0.4f, offset.z);
+      Vector3 position = new Vector3(target.position.x, 0.4f, offset.z);
+      Vector3 smoothed = Vector3.Lerp(position, desired, smoothSpeed);
+      transform.position = smoothed;
+    }
+
+}
---
+++ Assets/Scripts/PlayerTransform.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTransform : MonoBehaviour
+{
+
+    public BoxCollider2D boxCollider;
+    public Animator anim;
+
+    public BoxCollider2D cube;
+    public PolygonCollider2D triangle;
+
+    int state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        state = anim.GetInteger("State");
+
+        if(Input.GetKeyDown(KeyCode.E) && state < 1){
+          state ++;
+          anim.SetInteger("State", state);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Q) && state > 0){
+          state --;
+          anim.SetInteger("State", state);
+        }
+
+        switch(state)
+        {
+          case(0):
+            cube.enabled = true;
+            triangle.enabled = false;
+            break;
+          case(1):
+            cube.enabled = false;
+            triangle.enabled = true;
+            break;
+        }
+
+    }
+}
