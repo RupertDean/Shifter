19bcc2785b9e060487decd980b6998a6db62d8f2
Rupert Dean <deanrupert@outlook.com>
2021-04-26 23:37:49 +0100

    Added circle to player

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -3,15 +3,12 @@
 public class CameraFollow : MonoBehaviour
 {
     public Transform target;
-    public float smoothSpeed = 0.01f;
     public Vector3 offset;
 
     void LateUpdate()
     {
-      Vector3 desired = new Vector3(target.position.x + offset.x, 0.4f, offset.z);
-      Vector3 position = new Vector3(target.position.x, 0.4f, offset.z);
-      Vector3 smoothed = Vector3.Lerp(position, desired, smoothSpeed);
-      transform.position = smoothed;
+        Vector3 desired = new Vector3(target.position.x + offset.x, 0.4f, offset.z);
+        transform.position = desired;
     }
 
 }
--- Assets/Scripts/PlayerTransform.cs
+++ Assets/Scripts/PlayerTransform.cs
@@ -10,8 +10,9 @@ public class PlayerTransform : MonoBehaviour
 
     public BoxCollider2D cube;
     public PolygonCollider2D triangle;
+    public CircleCollider2D circle;
 
-    int state;
+    int state = 1;
 
     // Start is called before the first frame update
     void Start()
@@ -24,7 +25,7 @@ public class PlayerTransform : MonoBehaviour
     {
         state = anim.GetInteger("State");
 
-        if(Input.GetKeyDown(KeyCode.E) && state < 1){
+        if(Input.GetKeyDown(KeyCode.E) && state < 2){
           state ++;
           anim.SetInteger("State", state);
         }
@@ -37,12 +38,17 @@ public class PlayerTransform : MonoBehaviour
         switch(state)
         {
           case(0):
-            cube.enabled = true;
-            triangle.enabled = false;
-            break;
-          case(1):
             cube.enabled = false;
             triangle.enabled = true;
+            break;
+          case(1):
+            cube.enabled = true;
+            circle.enabled = false;
+            triangle.enabled = false;
+            break;
+          case(2):
+            circle.enabled = true;
+            cube.enabled = false;
             break;
         }
 
